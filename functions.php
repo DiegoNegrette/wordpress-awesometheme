a701107a6918ea7db57786ac0bda6391e8acfc23
Diego Negrette <diegonegrette42@gmail.com>
2020-12-13 18:00:03 -0400

    El primer commit de mi tema de wordpress

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function awesome_script_enqueue() {
+
+    wp_enqueue_style('customstyle-css', get_template_directory_uri(), $deps:array, $ver:string|boolean|null, $media:string )
+
+}
+
+?>
