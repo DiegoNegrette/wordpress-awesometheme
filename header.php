fb897283dfdb2a64f9795b2d9a7c9b476d5eb535
Diego Negrette <diegonegrette42@gmail.com>
2020-12-13 22:51:00 -0400

    Codigo hasta el 5to video

--- header.php
+++ header.php
@@ -3,6 +3,17 @@
     <head>
         <meta charset="utf-8">
         <title>Awesome Theme</title>
+        <?php wp_head();?>
     <head>
 
-    <body>
+    <?php 
+        if( is_front_page() ):
+            $awesome_classes = array( 'awesome-class', 'my-class' );
+        else:
+            $awesome_classes = array( 'no-awesome-class' );
+        endif
+    ?>
+
+    <body <?php body_class( $awesome_classes ); ?>>
+
+        <?php wp_nav_menu( array( 'theme_location'=>'primary' ) ); ?>
